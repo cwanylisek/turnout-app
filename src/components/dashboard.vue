04bf241426ce74bad15bf580afa216ff0c712932
Bartek Tylkowski <30731599+cwanylisek@users.noreply.github.com>
2018-02-22 21:08:07 +0100

    event items update

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,14 +1,23 @@
 <template lang="html">
-  <div class="">
+  <div class="container col-md-4">
   <h3>Events Dashboard</h3>
   <button class="btn btn-danger btn-sm signout-btn" v-on:click="signOut">Sign Out</button>
-
-  {{$store.state}}
+  <hr>
+  <AddEvent />
+  <hr>
+  <div class="col-md-12">
+      <EventItem
+      v-for="(event_item, index) in this.$store.state.event"
+      :event="event_item"
+      key="index"
+  </div>
   </div>
 </template>
 
 <script>
-import { firebaseApp} from '../firebaseApp'
+import { firebaseApp, eventsRef } from '../firebaseApp'
+import AddEvent from './event.vue'
+import EventItem from './eventitem'
 
 export default {
   methods: {
@@ -16,6 +25,20 @@ export default {
       this.$store.dispatch('signOut')
       firebaseApp.auth().signOut()
     }
+  },
+  components: {
+    AddEvent,
+    EventItem
+  },
+  mounted() {
+    eventsRef.on('value', snap => {  // 'on' 2 argumenty
+      let events = []
+      snap.forEach(event => {
+        events.push(event.val())
+      })
+      this.$store.dispatch('setEvents', events)
+      console.log('events', events)
+    })
   }
 }
 </script>
--- src/store/actions.js
+++ src/store/actions.js
@@ -9,3 +9,7 @@ export const signIn = ({commit}, user_payload) => {
 export const signOut = ({commit}) => {
   commit(types.SIGN_OUT)
 }
+
+export const setEvents = ({commit}, events_payload) => {
+  commit(types.SET_EVENTS, events_payload)
+}
